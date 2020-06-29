35c293008e1fddf31c645ce7cb816da07da26610
Nareg Megan <nmegan@berkeley.edu>
2020-06-29 15:17:37 -0500

    init commit

---
+++ src/fpu/dev.v
@@ -0,0 +1,11 @@
+// Using IEEE-754 Single Precision Float
+
+module fp_gt (output out, input [31:0] f1, input [31:0] f2);
+
+    reg out = 1'b0;
+
+    always @ (*)
+    begin 
+
+    end
+endmodule
